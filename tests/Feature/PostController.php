064c5c867946dc82d70034d70465aa8278e01424
Chris Wharton <chriswharton@catalyst.net.nz>
2018-07-27 15:31:28 +1200

    Add Feature test for PostController

---
+++ tests/Feature/PostController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+
+
+class PostController extends TestCase
+{
+    use RefreshDatabase;
+
+    private $user;
+
+    private $existingPost;
+
+    /**
+     * Add a user and existing post.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+
+        $this->existingPost = factory(\App\Post::class)->create();
+    }
+
+    /**
+     * Test the index page route
+     */
+    public function testIndex()
+    {
+        $this->get('/')
+            ->assertSuccessful()
+            ->assertViewIs('post.index');
+    }
+
+    /**
+     *
+     */
+    public function testShow()
+    {
+        // TODO
+//        dd($this->existingPost);
+        // $response = $this->get("/post/{$this->existingPost['id']}")->assertViewIs('post.show');
+    }
+
+    /**
+     * Test the Create form loads.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $response = $this->get('/post')->assertViewIs('post.create');
+    }
+
+    /**
+     * Save a post.
+     */
+    public function testStore()
+    {
+        $data = [
+            'title' => 'test title',
+            'body' => 'test body',
+            'user_id' => $this->user->id
+        ];
+
+        $valid = $this->actingAs($this->user)
+            ->post('/post', $data)
+            ->assertRedirect('/');
+    }
+}

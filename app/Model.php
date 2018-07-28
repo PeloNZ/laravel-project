ed1256cfd4f7dcd3a140a7bc2b682e045170582e
Chris Wharton <chriswharton@catalyst.net.nz>
2018-07-28 18:04:50 +1200

    Set Model and User classes to explicitly use mysql

--- app/Model.php
+++ app/Model.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Model extends Eloquent
 {
+    protected $connection = 'mysql';
+
     // Allow these fields to be saved.
     protected $fillable = ['title', 'body', 'user_id'];
     

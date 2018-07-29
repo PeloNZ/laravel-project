15837293d44c837f78290cccf6e89f194d294109
Chris Wharton <chriswharton@catalyst.net.nz>
2018-07-29 21:23:55 +1200

    Improve post body input form

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,26 +1,25 @@
 @extends ('layouts.main') @section ('content')
 
-<h1>Create post</h1>
+    <h1>Create post</h1>
 
-<form method="POST" action="/post">
-    {{ csrf_field() }}
-    <div class="form-group">
-        <label for="title">Title</label> <input type="text"
-            class="form-control" id="title" placeholder="Title"
-            name="title">
-    </div>
-    <div class="form-group">
-        <label for="body">Body</label> <input type="textarea"
-            class="form-control" id="body" placeholder="Body"
-            name="body">
-    </div>
-    <div class="form-group">
-        <button type="submit" class="btn btn-default">Publish</button>
-    </div>
+    <form method="POST" action="/post">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="title">Title</label> <input type="text"
+                                                    class="form-control" id="title" placeholder="Title"
+                                                    name="title">
+        </div>
+        <div class="form-group">
+            <label for="body">Body</label>
+            <textarea class="form-control" id="body" rows="10" placeholder="Body" name="body"></textarea>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Publish</button>
+        </div>
 
-    <!-- validation -->
-    @include ('layouts.errors')
+        <!-- validation -->
+        @include ('layouts.errors')
 
-</form>
+    </form>
 
 @endsection
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,28 +1,28 @@
 @extends ('layouts.main') @section ('content')
 
-<h1>Edit post</h1>
+    <h1>Edit post</h1>
 
-<!-- validation -->
-@include ('layouts.errors')
-@include ('layouts.messages')
+    <!-- validation -->
+    @include ('layouts.errors')
+    @include ('layouts.messages')
 
-<form method="POST" action="/post/{{ $post->id }}">
-    {{ csrf_field() }}
-    {{ method_field('PATCH') }}
-    <div class="form-group">
-        <label for="title">Title</label> <input type="text"
-            class="form-control" id="title" placeholder="Title"
-            name="title" value="{{ $post->title }}">
-    </div>
-    <div class="form-group">
-        <label for="body">Body</label> <input type="textarea"
-            class="form-control" id="body" placeholder="Body"
-            name="body" value="{{ $post->body }}">
-    </div>
-    <div class="form-group">
-        <button type="submit" class="btn btn-default">Update</button>
-    </div>
+    <form method="POST" action="/post/{{ $post->id }}">
+        {{ csrf_field() }}
+        {{ method_field('PATCH') }}
+        <div class="form-group">
+            <label for="title">Title</label> <input type="text"
+                                                    class="form-control" id="title" placeholder="Title"
+                                                    name="title" value="{{ $post->title }}">
+        </div>
+        <div class="form-group">
+            <label for="body">Body</label>
+            <textarea class="form-control" id="body" rows="10" placeholder="Body" name="body"
+                      value="{{ $post->body }}"></textarea>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Update</button>
+        </div>
 
-</form>
+    </form>
 
 @endsection
